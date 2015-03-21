0a0b5770ccf56aeb66b3f3136672b46858ca4190
vaimer <overespada@gmail.com>
2015-03-21 16:10:47 +0300

    Working Likes Dao, change hibernate-context.xml

--- web/src/main/java/vk/java/api/TestingHibernateConnectionClass.java
+++ web/src/main/java/vk/java/api/TestingHibernateConnectionClass.java
@@ -1,7 +1,9 @@
 package vk.java.api;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import vk.java.api.persistence.dao.LikesDao;
 import vk.java.api.persistence.dao.PersonDao;
+import vk.java.api.persistence.domain.Likes;
 import vk.java.api.persistence.domain.Person;
 
 /**
@@ -19,15 +21,25 @@ public class TestingHibernateConnectionClass
                 new String[] { "applicationContext.xml" }, true);
         PersonDao dao = (PersonDao) context.getBean("PersonDao");
 
-        Person data = new Person("Sam2", "Daniels2")
-                .setBirthday("33.33.33")
-                .setNickName("VaimeR")
+        Person data = new Person("Sam", "Daniels")
+                .setBirthday("22.22.22")
+                .setNickName("Blind")
                 .setPhoto("http://google.com/photo/?sda#@wd2@#$$5")
                 .setSex(1L);
 
         Long id = dao.add(data);
 
         System.out.println (dao.get(id).getNickName());
+
+        LikesDao likesDao = (LikesDao) context.getBean("LikesDao");
+
+        Likes data1 = new Likes();
+        data1.setAmount(1000L);
+        data1.setPersonId(1L);
+
+        Long id1 = likesDao.add(data1);
+
+        System.out.println ("Amout of like" + likesDao.get(id1).getAmount());
     }
 
 }
